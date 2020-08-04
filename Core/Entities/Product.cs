f163e3c202e9bfcd6ab2f9f180aaed58d1f62663
Ahmad Kandil <A77mad.22andil@gmail.com>
2020-08-04 18:51:42 +0200

    Core & Infrastructure

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
