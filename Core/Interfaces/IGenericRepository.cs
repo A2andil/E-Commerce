bd01d09bfe94056350dae66c278b1bb4cc6a4866
A2andil <A77mad.22andil@gmail.com>
2020-08-13 01:58:20 +0200

    Editing IGenericRepository

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Core.Specification;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,5 +12,9 @@ namespace Core.Interfaces
         Task<T> GetByIdAsync(int id);
 
         Task<IReadOnlyList<T>> ListAllAsync();
+
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
     }
 }
