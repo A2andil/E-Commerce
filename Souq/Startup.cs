3a7209c0af0dadc5cae6cdd9257f8ca4acbd1686
A2andil <A77mad.22andil@gmail.com>
2020-08-22 05:47:12 +0200

    Adding Cors

--- Souq/Startup.cs
+++ Souq/Startup.cs
@@ -32,6 +32,15 @@ namespace Souq
 
             services.AddSwaggerDocumentation();
 
+            services.AddCors(opt =>
+               opt.AddPolicy("CorsPolicy", policy =>
+               {
+                   policy.AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .WithOrigins("https://localhost:4200");
+               });
+            ;
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
