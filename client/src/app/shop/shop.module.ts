6e35f6922acf7cacf85d7d92251761dc2b17a81e
A2andil <A77mad.22andil@gmail.com>
2020-08-31 08:21:14 +0200

    Services

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
-
+import { ShopService  } from "./shop.service";
 
 
 @NgModule({
   declarations: [ShopComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ShopService
   ]
 })
 export class ShopModule { }
