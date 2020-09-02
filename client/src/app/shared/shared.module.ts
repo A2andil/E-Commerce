ce7dbd8278d883b4dfc743f158e6c89d2eba92a0
A2andil <A77mad.22andil@gmail.com>
2020-09-02 18:27:40 +0200

    Pagination

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { PaginationModule } from 'ngx-bootstrap';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    //PaginationModule.forRoot()
+    PaginationModule.forRoot()
   ],
-  //exports: [PaginationModule]
+  exports: [PaginationModule]
 })
 export class SharedModule { }
