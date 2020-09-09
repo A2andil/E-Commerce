26aa46801c0ac8061366339758f64d98d2acd602
A2andil <A77mad.22andil@gmail.com>
2020-09-09 22:06:15 +0200

    Toast notification

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import {ToastrModule} from 'ngx-toastr';
 import { ServerErrorComponent } from './server-error/server-error.component';
 
 
@@ -12,7 +13,11 @@ import { ServerErrorComponent } from './server-error/server-error.component';
   declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [NavBarComponent]
 })
