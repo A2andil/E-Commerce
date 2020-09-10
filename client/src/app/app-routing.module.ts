6f7c9802fd3e4bf49d46da10059c0cc277cc5ca9
A2andil <A77mad.22andil@gmail.com>
2020-09-10 13:39:01 +0200

    breadcrumbs

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { NotFoundComponent } from './core/not-found/not-found.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-errors', component: TestErrorComponent},
-  {path: 'server-errors', component: ServerErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
+  {path: 'test-errors', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
+  {path: 'server-errors', component: ServerErrorComponent, data: {breadcrumb: 'Server Errors'}},
+  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
+  data: {breadcrumb: 'shop'}},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
